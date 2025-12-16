4c92c8ee2c5b622d5e233e89ab605d1272578ddf
Nilson Silva <nilsonsilva.ads00@gmail.com>
2025-12-15 21:30:10 -0300

    Foi adicionado a pagina topic no app_learninglogs.

--- app_learning_logs/views.py
+++ app_learning_logs/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     """ Pagina principal do Learning_log """
     return render(request, 'app_learning_logs/index.html')
+
+def topics(request):
+    """ Mostra todos os assuntos """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'app_learning_logs/topics.html', context)
